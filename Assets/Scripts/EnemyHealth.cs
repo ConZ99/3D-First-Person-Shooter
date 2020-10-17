6f2f2146e2268d15d2c04f2bad534a9ec2c8f861
Stefan Lica <stefanlica99@gmail.com>
2020-10-17 17:03:10 +0300

    StefanPack

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+    [SerializeField] int enemyType = 0;
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        BroadcastMessage("OnHitProvoke");
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            UnityEngine.Debug.Log("DEAD");
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        if (enemyType == 0)
+        {
+            //Collider colider;
+            //colider = GameComponent<Collider>();
+            gameObject.GetComponent<Collider>().enabled = false;
+            GetComponent<Animator>().SetTrigger("Die");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+    [SerializeField] TextMeshProUGUI healthText;
+
+    void Update()
+    {
+        DisplayHealth();
+    }
+
+    private void DisplayHealth()
+    {
+        healthText.text = hitPoints.ToString();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            UnityEngine.Debug.Log("DEAD");
+            GetComponent<DeathHandler>().HandleDeath();
+            //respawn menu
+        }
+    }
+}
