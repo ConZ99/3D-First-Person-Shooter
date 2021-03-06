6dbe1a1d1a0f70ccb4925c4c211058043339ea60
ConZ99 <stefanlica99@gmail.com>
2021-03-06 10:36:22 +0200

    Part 2: Quest Menu, apasa K

---
+++ Assets/Game/Scripts/QuestMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuestMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject questMenu;
+
+    private void Start()
+    {
+        questMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            if (isPaused == false)
+            {
+                questMenu.SetActive(true);
+                isPaused = true;
+            }
+            else
+            {
+                questMenu.SetActive(false);
+                isPaused = false;
+            }
+        }
+    }
+}
